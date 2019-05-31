689e5728fb32ad40a5156409703bae4cd1d66533
Natacha Bourg <n.bourg@unsw.edu.au>
2019-05-31 18:02:39 +1000

    Change files to use parcels in notebook and spyder

--- imos_current_data/analysis_imos_current_data/plot_trajectory_imos.py
+++ imos_current_data/analysis_imos_current_data/plot_trajectory_imos.py
@@ -34,14 +34,14 @@ def PlotImosFile(filenames):
         particle.delete()
     images = []
     fieldset = FieldSet.from_netcdf(filenames, variables, dimensions)
-    """pset = ParticleSet.from_line(fieldset=fieldset,   # the fields on which the particles are advected
+    pset = ParticleSet.from_line(fieldset=fieldset,   # the fields on which the particles are advected
                                 pclass=JITParticle,
                                 size=10,
                                 start=(151.259, -33.919),
                                 finish=(151.257,-33.923))
                               #  start=(152., -30),
                                # finish=(159.,-34))
-    """
+    
     pset=ParticleSet(fieldset=fieldset, pclass=JITParticle, lon=151.259, lat=-33.92, time=datetime(2019, 3, 9, 2))
 
     k_WestVel = pset.Kernel(WestVel)
@@ -51,15 +51,17 @@ def PlotImosFile(filenames):
                     dt=-timedelta(minutes=5),      # the timestep of the kernel
                     recovery={ErrorCode.ErrorOutOfBounds: DeleteParticle},
                     output_file=pset.ParticleFile(name="IMOSTRY.nc", outputdt=timedelta(hours=1)))
-        pset.show(domain={'N':-15, 'S':-42, 'E':170, 'W':140}, field='vector',vmin=0.,vmax=1.3, savefile='../outputs_imos_current_data/make_gif/particl_revers'+str(i)+'.png')
+        
+       """ pset.show(domain={'N':-15, 'S':-42, 'E':170, 'W':140}, field='vector',vmin=0.,vmax=1.3, savefile='../outputs_imos_current_data/make_gif/particl_revers'+str(i)+'.png')
         images.append(imageio.imread('../outputs_imos_current_data/make_gif/particl_revers'+str(i)+'.png'))
     imageio.mimsave('../outputs_imos_current_data/particle_advection_reverse.gif', images)
     
    #cpt.plotTrajectoriesFileModified('IMOSTRY.nc',mode='movie2d', tracerlon='LONGITUDE',tracerlat='LATITUDE',tracerfield='UCUR');
-    """tracerfile=filenames,
+   tracerfile=filenames,
                                      tracerlon='LONGITUDE',
                                      tracerlat='LATITUDE',
                                      tracerfield='UCUR');"""
+
 
 filenames = "../raw_imos_current_data/IMOS_OceanCurrent_HV_2019_C-20190520T232835Z.nc"
 PlotImosFile(filenames)
